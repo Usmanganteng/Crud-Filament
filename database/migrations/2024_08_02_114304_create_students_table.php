2ea78c0048ab1a07d415ef6fa0fb94e2aa512a74
Muhamad Aldizar Ilham <ilhamaldizar@gmail.com>
2024-10-01 10:17:02 +0700

    Create Crud Students In Filament

--- database/migrations/2024_08_02_114304_create_students_table.php
+++ database/migrations/2024_08_02_114304_create_students_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('nim')->unique();
+            $table->string('nama');
+            $table->enum('fakultas', ['MIPA', 'FEBI', 'SOSHUM']);
             $table->timestamps();
         });
     }
